ae324a1b21421a73d3e51da0ed00a8c8c4e541d8
DevanshGupta7 <devansh7gupta@gmail.com>
2023-12-24 21:27:18 +0530

    fixes bugs

--- choose_plan.py
+++ choose_plan.py
@@ -3,55 +3,58 @@ import time
 
 
 def plan_choose(memberships_plans, costmr_id):
+    print("You dont have any valid membership pls buy to continue")
+    
+    from table_print import print_table_plan
+    print_table_plan(memberships_plans)
+    print("\n")
+    print("Deluxe:\nAccess to gym facilities, Standard workout equipment usage (cardio machines, weightlifting equipment), general fitness classes\n\nSuper:\nAll benefits of Deluxe Membership, Access to additional amenities, such as sauna or steam room, Extended gym hours, Personalized fitness assessment\n\nPremium:\nAll benefits of Super Membership, Access to premium gym equipment and technology, Unlimited access to all fitness classes, including specialty classes, Personalized workout plans designed by fitness trainers, Nutrition consultation")
+    print("NO REFUND POLICY")
+    
     while True:
-        print("You dont have any valid membership pls buy to continue")
-        from table_print import print_table_plan
-        print_table_plan(memberships_plans)
-        print("\n")
-        print("Deluxe:\nAccess to gym facilities, Standard workout equipment usage (cardio machines, weightlifting equipment), general fitness classes\n\nSuper:\nAll benefits of Deluxe Membership, Access to additional amenities, such as sauna or steam room, Extended gym hours, Personalized fitness assessment\n\nPremium:\nAll benefits of Super Membership, Access to premium gym equipment and technology, Unlimited access to all fitness classes, including specialty classes, Personalized workout plans designed by fitness trainers, Nutrition consultation")
-        print("NO REFUND POLICY")
-        
-        while True:
-            print("***Welcome to GYM***\n")
-            try:
-                plan = int(input("1. Deluxe\n2. Super\n3. Premium\n"))
-                if plan > 3:
-                    print("Pls choose only from given options")
-                    time.sleep(2)
-                    clear_terminal()
-                    continue
-                else:
-                    clear_terminal()
-                    break
-            except ValueError:
-                print("Pls input integer for choice. Wait for 2 seconds")
+        print("***Welcome to GYM***\n")
+        try:
+            plan = int(input("1. Deluxe\n2. Super\n3. Premium\n"))
+            if plan > 3:
+                print("Pls choose only from given options")
                 time.sleep(2)
                 clear_terminal()
+                continue
+            else:
+                clear_terminal()
+                break
+        except ValueError:
+            print("Pls input integer for choice. Wait for 2 seconds")
+            time.sleep(2)
+            clear_terminal()
+    
+    if plan == 1:
+        confirm = input("Do want to confirm it")
+        if confirm.upper() == "YES":
+            month = int(input("Duration of membership in month: "))
+            money_pay = (800*month) - (800*month)*((month*month)/100)
+            print(f"Original money is {month*800}\nDiscounted money is {money_pay}")
+        else:
+            plan_choose()
+            return
+    elif plan == 2:
+        confirm = input("Do want to confirm it")
+        if confirm.upper() == "YES":
+            month = int(input("Duration of membership in month: "))
+            money_pay = (1500*month) - (1500*month)*((month*month)/100)
+        else:
+            plan_choose()
+            return
+        print(f"Original money is {month*1500}\nDiscounted money is {money_pay}")
+    elif plan == 3:
+        confirm = input("Do want to confirm it")
+        if confirm.upper() == "YES":
+            month = int(input("Duration of membership in month: "))
+            money_pay = (2000*month) - (2000*month)*((month*month)/100)
+            print(f"Original money is {month*2000}\nDiscounted money is {money_pay}")
+        else:
+            plan_choose()
+            return
         
-        if plan == 1:
-            confirm = input("Do want to confirm it")
-            if confirm.upper() == "YES":
-                month = int(input("Duration of membership in month: "))
-                money_pay = (800*month) - (800*month)*((month*month)/100)
-                print(f"Original money is {month*800}\nDiscounted money is {money_pay}")
-            else:
-                continue
-        elif plan == 2:
-            confirm = input("Do want to confirm it")
-            if confirm.upper() == "YES":
-                month = int(input("Duration of membership in month: "))
-                money_pay = (1500*month) - (1500*month)*((month*month)/100)
-            else:
-                continue
-            print(f"Original money is {month*1500}\nDiscounted money is {money_pay}")
-        elif plan == 3:
-            confirm = input("Do want to confirm it")
-            if confirm.upper() == "YES":
-                month = int(input("Duration of membership in month: "))
-                money_pay = (2000*month) - (2000*month)*((month*month)/100)
-                print(f"Original money is {month*2000}\nDiscounted money is {money_pay}")
-            else:
-                continue
-            
-        from choice_payment import payment_choice
-        payment_choice(costmr_id, money_pay, month)
+    from choice_payment import payment_choice
+    payment_choice(costmr_id, money_pay, month)
--- customer_login.py
+++ customer_login.py
@@ -7,105 +7,99 @@ from datetime import datetime
 
 def choose_customer():
     while True:
-        while True:
-            print("***Welcome to GYM***\n")
-            try:
-                choice = input(f"1. Existing costumer?\n2. New costumer?\n(Enter 'BACK' for going back)\n")
-                if choice.upper() == "BACK":
-                    from user_choose import choose_user
-                    choose_user()
-                else:
-                    choice = int(choice)
-                if choice > 2:
-                    print("Pls choose only from given options")
-                    time.sleep(2)
-                    clear_terminal()
-                    continue
-                else:
-                    clear_terminal()
-                    break
-            except ValueError:
-                print("Pls input integer for choice. Wait for 2 seconds")
+        print("***Welcome to GYM***\n")
+        try:
+            choice = input(f"1. Existing costumer?\n2. New costumer?\n(Enter 'BACK' for going back)\n")
+            if choice.upper() == "BACK":
+                from user_choose import choose_user
+                choose_user()
+            else:
+                choice = int(choice)
+            if choice > 2:
+                print("Pls choose only from given options")
                 time.sleep(2)
                 clear_terminal()
+                continue
+            else:
+                clear_terminal()
+                break
+        except ValueError:
+            print("Pls input integer for choice. Wait for 2 seconds")
+            time.sleep(2)
+            clear_terminal()
+            
+            
+    if choice == 1:
+        is_costumer_valid = False
+        costmr_id = int(input("Enter your id: "))
+        back_continue = int(input("1. Continue\n2. Back\n"))
+
+        if back_continue == 2:
+            clear_terminal()
+            choose_customer()
+            return
+        if costmr_id in memberships:
+            if memberships[costmr_id]["end_date"] < datetime.now().date().strftime("%Y-%m-%d") or len(memberships[costmr_id]["end_date"]) == 0:
+                memberships[costmr_id]["Membership"] = "NO"
                 
+                if memberships[costmr_id]["Membership"] == "NO":
+                    from choose_plan import plan_choose
+                    plan_choose(memberships_plans, costmr_id)
+                else:
+                    print("Welcome to GYM")
+                    is_costumer_valid = True
+                    
+            else:
+                print("Welcome to GYM")
+                is_costumer_valid = True
                 
-        if choice == 1:
-            is_costumer_valid = False
-            while True:
-                costmr_id = input("Enter your id (Enter 'BACK' for going back): ")
-                if costmr_id.upper() == "BACK":
-                    clear_terminal()
+        else:
+            print("Pls enter correct ID or register to our GYM")
+            choose_customer()
+            return
+            
+        if is_costumer_valid:
+            print("You have valid membership. So, please continue to GYM")
+            
+    elif choice == 2:
+        print("Register yourself in our GYM\n")
+        name = input("Enter your name: ")
+        address = input("Enter your address: ")
+
+        while True:
+            try:
+                contact = int(input("Enter your contact number: "))
+                back_continue = int(input("1. Continue\n2. Back\n"))
+                if back_continue == 1:
                     break
-                else:
-                    costmr_id = int(costmr_id)
-                if costmr_id in memberships:
-                    if memberships[costmr_id]["end_date"] < datetime.now().date():
-                        memberships[costmr_id]["Membership"] = "NO"
-                        
-                        if memberships[costmr_id]["Membership"] == "NO":
-                            from choose_plan import plan_choose
-                            plan_choose(memberships_plans, costmr_id)
-                        else:
-                            print("Welcome to GYM")
-                            is_costumer_valid = True
-                            break
-                    else:
-                        print("Welcome to GYM")
-                        is_costumer_valid = True
-                        break
-                else:
-                    print("Pls enter correct ID or register to our GYM")
+                elif back_continue == 2:
+                    clear_terminal()
+                    choose_customer()
+                    return
+                
+                if len(str(contact)) != 10:
+                    print("Pls enter 10-digit mobile number!")
                     continue
-                
-            if is_costumer_valid:
-                print("You have valid membership. So, please continue to GYM")
-                
-        elif choice == 2:
-            print("Register yourself in our GYM\n")
-            while True:
-                name = input("Enter your name(Enter 'BACK' for going back): ")
-                if name.upper() == "BACK":
-                    clear_terminal()
-                    break
-                address = input("Enter your address(Enter 'BACK' for going back): ")
-                if address.upper() == "BACK":
-                    clear_terminal()
+                else:
                     break
 
-                while True:
-                    try:
-                        contact = (input("Enter your contact number(Enter 'BACK' for going back): "))
-                        if contact.upper() == "BACK":
-                            clear_terminal()
-                            return
-                        else:
-                            contact = int(contact)
-                        if len(str(contact)) != 10:
-                            print("Pls enter 10-digit mobile number!")
-                            continue
-                        else:
-                            break
+            except ValueError:
+                print("Pls enter only integers")
+                continue
 
-                    except ValueError:
-                        print("Pls enter only integers")
-                        continue
-
-                while True:
-                    is_signedUp = False
-                    costumer_ID = random.randint(100000, 999999)
-                    if costumer_ID not in costumer_details:
-                        costumer_details[costumer_ID] = {
-                            "name": name, "address": address, "contact": contact}
-                        memberships[costumer_ID] = {
-                            "Membership": "NO", "plan":"", "start_date": "", "end_date": ""}
-                        from table_print import print_table_costumer
-                        is_signedUp = True
-                        print_table_costumer(costumer_details, costumer_ID)
-                        enter = input("Press any key and tap enter to continue: ")
-                        if len(enter) != 0:
-                            break
-                if is_signedUp:
-                    break
+        while True:
+            costumer_ID = random.randint(100000, 999999)
+            if costumer_ID not in costumer_details:
+                costumer_details[costumer_ID] = {
+                    "name": name, "address": address, "contact": contact}
+                memberships[costumer_ID] = {
+                    "Membership": "NO", "plan":"", "start_date": "", "end_date": ""}
+                from table_print import print_table_costumer
+                print_table_costumer(costumer_details, costumer_ID)
+                print(memberships)
+                enter = input("Press any key and tap enter to continue: ")
+                if len(enter) != 0:
+                    choose_customer()
+                    return
             
             
